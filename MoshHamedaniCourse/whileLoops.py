12673439b058a4382960b0833455d5d32400ac96
Owain Williams <owaingdwilliams@gmail.com>
2019-06-22 18:34:07 +0100

    While loops
    Creating a couple of while loop games.

---
+++ MoshHamedaniCourse/whileLoops.py
@@ -0,0 +1,20 @@
+i = 1
+while i <= 5:
+    print('*' * i)
+    i = i + 1
+
+
+
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input("Guess a number: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+    print("Sorry, you failed!")
+
