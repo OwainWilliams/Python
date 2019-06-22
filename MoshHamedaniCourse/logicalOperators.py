ce30cbf0c825d8898afc0258278317d5dea9234c
Owain Williams <owaingdwilliams@gmail.com>
2019-06-22 18:05:54 +0100

    Course update
    Adding example code from what I've covered so far from the course

---
+++ MoshHamedaniCourse/logicalOperators.py
@@ -0,0 +1,13 @@
+has_high_income = True
+has_good_credit = True
+has_criminal_record = False
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+
+if has_high_income or has_good_credit:
+    print("Eligible for loan.")
+
+if has_good_credit and not has_criminal_record:
+    print("Give them a loan.")
---
+++ MoshHamedaniCourse/operatorPrecedence.py
@@ -0,0 +1,17 @@
+x = 10 + 3 * 2 # 16
+print(x)
+
+# Multiplier has a higher precendenc that addition. 
+# this means 3 x 2 = 6 + 10 = 16
+
+# The order of operations
+# parenthesis
+# exponentiation 2 ** 3 
+# multiplication or division
+# addition or subtraction
+
+x = 10 + 3 * 2 ** 2 # 22
+print (x)
+
+x = (2 + 3) * 10 - 3 
+print (x)
