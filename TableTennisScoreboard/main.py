7c455088b8eb86f39fe2e02b7b37064cde9e2e1f
Owain Williams <owain@lewis.co.uk>
2019-07-14 21:38:02 +0100

    pyPingPongScorer
    A python score keeper built in Python

--- TableTennisScoreboard/main.py
+++ TableTennisScoreboard/main.py
@@ -1,43 +1,71 @@
-# Increment 2 numbers by 1. If user presses 1 add 1 to player one's score
-# If user presses 2, add 1 to player two's score
 
- 
 def main():
     print('Welcome to Table Tennis Scoreboard V1.0')
     startSinglesGame()
-
-def playerScores(whoScored):
-    if whoScored == '1':
-        global playerOneScore
-        playerOneScore += 1
-    elif whoScored == '2':
-        global playerTwoScore
-        playerTwoScore += 1
-        
-    print('The score is: '+ str(playerOneScore) + ' / ' + str(playerTwoScore))
 
 
 def startSinglesGame():
     global playerOneScore
     global playerTwoScore
-    gameStarter = False
-
-
+   
     while True:
-        prompt = input('Press 1 when Player 1 scores, Press 2 when Player 2 scores :')
-        if prompt == '1' or prompt == '2':
-            if playerOneScore < 11 or playerTwoScore < 11:
-                playerScores(prompt)
+        if playerOneScore < 11 and playerTwoScore < 11:
+            if playerOneScore == 10 and playerTwoScore == 10:
+                tiebreak()
             else:
-                if playerOneScore == 11:
-                    print('Playe One Wins!!!')
+                prompt = input('Press 1 when Player 1 scores, Press 2 when Player 2 scores :')
+                if prompt == '1' or prompt == '2':
+                    playerScores(prompt)
                 else:
-                    print('Player Two Wins!!!')  
+                    break
         else:
-            break
+            if playerOneScore == 11:
+                print('Player One wins!')
+                break
+            else:
+                print('Player Two wins!')
+                break
+            
+
+def playerScores(whoScored):
+    global pointsPlayed
+    global playerOneScore
+    global playerTwoScore
+
+    if whoScored == '1':
+        playerOneScore += 1
+    elif whoScored == '2':
+        playerTwoScore += 1
     
+    pointsPlayed += 1
+    print('The score is: '+ str(playerOneScore) + ' / ' + str(playerTwoScore))
+    
+    changeService = checkService(pointsPlayed)
+    if changeService == 'change':
+        print('Change service')
+
+
+def tiebreak():
+    print('Tie break!')
+    temp = 0
+
+
+def reset():
+    global playerOneScore
+    global playerTwoScore
+    playerOneScore = 0
+    playerTwoScore = 0
+
+
+
+def checkService(pointsPlayed):
+    if pointsPlayed % 2 == 0:
+        return('change')
+    else:
+        return ()
+
 
 playerOneScore = 0
 playerTwoScore = 0
-
+pointsPlayed = 0
 main()
